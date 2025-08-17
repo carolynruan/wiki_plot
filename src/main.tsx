cfcfadc20e4e0048ef1157f0a851022aa30fbdf3
carolynruan <98036925+carolynruan@users.noreply.github.com>
2025-08-16 22:43:35 -0700

    try again

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './styles/article.css';
+import './styles/Article.css';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
